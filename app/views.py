204d03ba94aa321d4700fcbc26961a298b923db2
SvyatoslavRsh <94186408+SvyatoslavRsh@users.noreply.github.com>
2021-11-12 17:20:28 +0200

    lab4

--- app/views.py
+++ app/views.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 import os, sys, platform
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, Length, AnyOf
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'asdAWdfz21zasdasdaw64!'
 
 @app.route("/blog")
 def blog():
@@ -35,6 +38,25 @@ def aboutme():
     return render_template("aboutme.html", boolean=False, name='Svyatoslav', error='Wrong data',
                            sys_info=request.headers.get('User-Agent'), sys=sys.version, os_name=os.name,
                            platform=platform.system(), release=platform.release(), date=datetime.now())
+
+
+# Lab 4
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired('A username is required'),
+                                                   Length(min=5, max=10, message='Must be at least 5 and at most 10 '
+                                                                                 'characters')])
+    password = PasswordField('password',
+                             validators=[InputRequired('Password is required'), AnyOf(values=['password', 'secret'])])
+
+
+@app.route("/form", methods=['GET', 'POST'])
+def form():
+    form = LoginForm()
+    flash('password is password or secret')
+    if form.validate_on_submit():
+        return f'The username is {form.username.data}. The password is {form.password.data}'
+
+    return render_template('form.html', form=form)
 
 
 if __name__ == '__main__':
